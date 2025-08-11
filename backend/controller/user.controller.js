ff1315ef422fa7d71d1b861f3bcf132f2d7647e7
achint <achintj96244@gmail.com>
2025-08-11 19:39:36 +0530

    Add authentication middleware and implement login functionality in user controller

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -16,7 +16,6 @@ const register = async (req, res) => {
   const user = await createUser({
     firstname,
     lastname,
-
     email,
     password: hashpassword,
   });
@@ -25,5 +24,27 @@ const register = async (req, res) => {
 
   res.status(201).json({ user, token });
 };
+const loginUser = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { email, password } = req.body;
 
-export { register };
+  const user = await userModel.findOne({ email }).select("+password");
+
+  if (!user) {
+    return res.status(401).json({ message: "invalid email or password" });
+  }
+  const isMatched = await user.comparePassword(password);
+
+  if (!isMatched) {
+    return res.status(401).json({ message: "invalid email or password" });
+  }
+
+  const token = user.generateAuthToken();
+
+  res.status(200).json({ token, user });
+};
+
+export { register, loginUser };
