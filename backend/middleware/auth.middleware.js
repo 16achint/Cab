49c79c24e34099f6d8a7785e2e95d8481d0193aa
achint <achintj96244@gmail.com>
2025-08-12 00:03:31 +0530

    Enhance authentication middleware to check for blacklisted tokens

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,12 +1,20 @@
 import jwt from "jsonwebtoken";
 import userModel from "../models/user.model.js";
-import bcrypt from "bcrypt";
+import blacklistTokenSchema from "../models/blacklistToken.model.js";
 
 const authUser = async (req, res, next) => {
-  const token = req.cookies.token || req.headers.authorization.split(" ")[1];
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
   if (!token) {
     return res.status(401).json({ message: "unauthorized" });
   }
+
+  const isBlacklisted = await blacklistTokenSchema.findOne({ token: token });
+
+  if (isBlacklisted) {
+    return res.status(401).json({ message: "unauthorized" });
+  }
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await userModel.findById(decoded._id);
@@ -16,3 +24,5 @@ const authUser = async (req, res, next) => {
     res.status(401).json({ message: error.message });
   }
 };
+
+export { authUser };
