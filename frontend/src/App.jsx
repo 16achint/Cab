25b1b4b524d15f32577065b66467a105e930b50c
achint <achintj96244@gmail.com>
2025-08-13 22:31:52 +0530

    Add user logout route and protect home route with UserProtectWrapper

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,10 @@ import UserLogin from "./pages/Userlogin";
 import UserSignup from "./pages/UserSignup";
 import CaptionLogin from "./pages/CaptionLogin";
 import CaptainSignup from "./pages/CaptainSignup";
+import UserProctectWrapper from "./pages/UserProctectWrapper";
+import UserLogout from "./pages/UserLogout";
 import Home from "./pages/Home";
+import CaptainHome from "./pages/CaptainHome";
 import "./App.css";
 
 function App() {
@@ -14,9 +17,26 @@ function App() {
         <Route path="/" element={<Start />} />
         <Route path="/login" element={<UserLogin />} />
         <Route path="/signup" element={<UserSignup />} />
+        <Route
+          path="/home"
+          element={
+            <UserProctectWrapper>
+              <Home />
+            </UserProctectWrapper>
+          }
+        />
+        <Route
+          path="/logout"
+          element={
+            <UserProctectWrapper>
+              <UserLogout />
+            </UserProctectWrapper>
+          }
+        />
+
         <Route path="/captain-login" element={<CaptionLogin />} />
         <Route path="/captain-signup" element={<CaptainSignup />} />
-        <Route path="/Home" element={<Home />} />
+        <Route path="/captain-home" element={<CaptainHome />} />
       </Routes>
     </div>
   );
