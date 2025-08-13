816992f3dc8bfbf2ec2d61a65a8d8952e1e95c97
achint <achintj96244@gmail.com>
2025-08-13 22:31:57 +0530

    Wrap App component with UserContext and CaptainContext for improved context management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import UserContext from "./context/UserContext.jsx";
+import CaptainContext from "./context/CaptainContext.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <CaptainContext>
+    <UserContext>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UserContext>
+  </CaptainContext>
 );
