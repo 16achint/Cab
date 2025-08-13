ee89273baf98f685f57d30fbdabce485c46bc0c0
achint <achintj96244@gmail.com>
2025-08-13 22:32:22 +0530

    Refactor CaptionLogin component to utilize CaptainDataContext for improved state management

--- frontend/src/pages/CaptionLogin.jsx
+++ frontend/src/pages/CaptionLogin.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-
+import { CaptainDataContext } from "../context/CaptainContext";
 const Captainlogin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const { value } = useContext(CaptainDataContext);
   const submitHandler = async (e) => {
     e.preventDefault();
 
