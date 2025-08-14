1cecc16c3d5f846c86bdf48c8f59825a5d35c470
achint <achintj96244@gmail.com>
2025-08-15 01:00:06 +0530

    Refactor UserProtectWrapper to improve user context destructuring and enhance logging

--- frontend/src/wrapper/UserProctectWrapper.jsx
+++ frontend/src/wrapper/UserProctectWrapper.jsx
@@ -2,17 +2,19 @@ import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { userDataContext } from "../context/UserContext";
+
 function UserProctectWrapper({ children }) {
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(true);
-  const [user, setuser] = useContext(userDataContext);
+  const { user, setuser } = useContext(userDataContext);
 
   useEffect(() => {
     if (!token) {
       navigate("/login");
     }
 
+    console.log("hello check");
     axios
       .get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
         headers: {
@@ -21,12 +23,14 @@ function UserProctectWrapper({ children }) {
       })
       .then((response) => {
         if (response.status === 200) {
+          console.log("hello");
           const data = response.data;
           setuser(data.user);
           setIsLoading(false);
         }
       })
       .catch((error) => {
+        console.log("hello check error");
         localStorage.removeItem("token");
         navigate("/login");
       });
