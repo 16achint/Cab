29a450763261aecac2686d7f69dcb0929d4fc5f7
achint <achintj96244@gmail.com>
2025-08-14 18:46:26 +0530

    Refactor UserProtectWrapper to include user profile fetching and loading state management

--- frontend/src/wrapper/UserProctectWrapper.jsx
+++ frontend/src/wrapper/UserProctectWrapper.jsx
@@ -1,15 +1,36 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { userDataContext } from "../context/UserContext";
 function UserProctectWrapper({ children }) {
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setuser] = useContext(userDataContext);
 
   useEffect(() => {
     if (!token) {
       navigate("/login");
     }
-  }, [token, navigate]);
+
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          const data = response.data;
+          setuser(data.user);
+          setIsLoading(false);
+        }
+      })
+      .catch((error) => {
+        localStorage.removeItem("token");
+        navigate("/login");
+      });
+  }, [token]);
 
   return <div>{children}</div>;
 }
