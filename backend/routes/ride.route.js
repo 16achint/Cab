d709f680b4eda9a1174e361630462bea35189956
achint <achintj96244@gmail.com>
2025-08-17 20:30:36 +0530

    Refactor ride route to include authentication middleware and improve validation messages for ride creation

--- backend/routes/ride.route.js
+++ backend/routes/ride.route.js
@@ -1,12 +1,16 @@
 import express from "express";
 const route = express.Router();
 import { body } from "express-validator";
+import { createRide } from "../controller/ride.controller.js";
+import { authUser } from "../middleware/auth.middleware.js";
 
 route.post(
     "/create-ride",
-    body("userId").isString().isLength({ min: 24, max: 24 }).withMessage("invaild user id"),
+    authUser,
     body("pickup").isString().isLength({ min: 3 }).withMessage("invalid pickUp address"),
-    body("destination").isString().isLength({ min: 3 }).withMessage("invalid destination address")
+    body("destination").isString().isLength({ min: 3 }).withMessage("invalid destination address"),
+    body("vehicleType").isString().isIn(["car", "auto", "moto"]).withMessage("invalid vehicle Type"),
+    createRide
 );
 
 export default route;
