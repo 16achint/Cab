252922158bb29dc61ad98fc6fa45e5ee2bff4628
achint <achintj96244@gmail.com>
2025-08-17 16:11:17 +0530

    Refactor user route for improved readability and consistency in validation message formatting

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,5 @@
 import { body } from "express-validator";
-import {
-  register,
-  loginUser,
-  logoutUser,
-  getUserprofile,
-} from "../controller/user.controller.js";
+import { register, loginUser, logoutUser, getUserprofile } from "../controller/user.controller.js";
 import { authUser } from "../middleware/auth.middleware.js";
 
 import { Router } from "express";
@@ -12,27 +7,21 @@ import { Router } from "express";
 const router = Router();
 
 router.post(
-  "/register",
-  [
-    body("email").isEmail().withMessage("invaild email"),
-    body("fullname.firstname")
-      .isLength({ min: 3 })
-      .withMessage("first name must be 3 char"),
-    body("password")
-      .isLength({ min: 6 })
-      .withMessage("password must be 6 char atleast"),
-  ],
-  register
+    "/register",
+    [
+        body("email").isEmail().withMessage("invaild email"),
+        body("fullname.firstname").isLength({ min: 3 }).withMessage("first name must be 3 char"),
+        body("password").isLength({ min: 6 }).withMessage("password must be 6 char atleast"),
+    ],
+    register
 );
 router.post(
-  "/login",
-  [
-    body("email").isEmail().withMessage("invaild email"),
-    body("password")
-      .isLength({ min: 6 })
-      .withMessage("password must be 6 char atleast"),
-  ],
-  loginUser
+    "/login",
+    [
+        body("email").isEmail().withMessage("invaild email"),
+        body("password").isLength({ min: 6 }).withMessage("password must be 6 char atleast"),
+    ],
+    loginUser
 );
 
 router.get("/profile", authUser, getUserprofile);
