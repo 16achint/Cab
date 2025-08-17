20fc26da21a57dbe9b62a297bc139eb0f90a8a9a
achint <achintj96244@gmail.com>
2025-08-17 16:30:31 +0530

    Refactor user model for improved readability and consistency in schema formatting

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,46 +2,46 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 const userSchema = new mongoose.Schema({
-  fullname: {
-    firstname: {
-      type: String,
-      require: true,
-      minlength: [3, "First name must be length of 6 char"],
+    fullname: {
+        firstname: {
+            type: String,
+            require: true,
+            minlength: [3, "First name must be length of 6 char"],
+        },
+        lastname: {
+            type: String,
+            require: true,
+            minlength: [3, "Last name must be length of 6 char"],
+        },
     },
-    lastname: {
-      type: String,
-      require: true,
-      minlength: [3, "Last name must be length of 6 char"],
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+        minlength: [5, "email must be length of 5 char"],
     },
-  },
-  email: {
-    type: String,
-    require: true,
-    unique: true,
-    minlength: [5, "email must be length of 5 char"],
-  },
-  password: {
-    type: String,
-    require: true,
-    select: false,
-  },
+    password: {
+        type: String,
+        require: true,
+        select: false,
+    },
 
-  socketId: {
-    type: String,
-  },
+    socketId: {
+        type: String,
+    },
 });
 
 userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
-  return token;
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
+    return token;
 };
 
 userSchema.methods.comparePassword = async function (password) {
-  return await bcrypt.compare(password, this.password);
+    return await bcrypt.compare(password, this.password);
 };
 
 userSchema.statics.hashPassword = async function (password) {
-  return await bcrypt.hash(password, 10);
+    return await bcrypt.hash(password, 10);
 };
 
 const userModel = mongoose.model("User", userSchema);
