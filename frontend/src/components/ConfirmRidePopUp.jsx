06142c512f20d38ed8ec44f8530bb99851665d8e
achint <achintj96244@gmail.com>
2025-08-17 14:11:57 +0530

    Refactor ConfirmRidePopUp component to remove API call and replace dynamic data with static values

--- frontend/src/components/ConfirmRidePopUp.jsx
+++ frontend/src/components/ConfirmRidePopUp.jsx
@@ -1,33 +1,14 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 const ConfirmRidePopUp = (props) => {
   const [otp, setOtp] = useState("");
-  const navigate = useNavigate();
+
 
   const submitHander = async (e) => {
     e.preventDefault();
 
-    const response = await axios.get(
-      `${import.meta.env.VITE_BASE_URL}/rides/start-ride`,
-      {
-        params: {
-          rideId: props.ride._id,
-          otp: otp,
-        },
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      }
-    );
 
-    if (response.status === 200) {
-      props.setConfirmRidePopupPanel(false);
-      props.setRidePopupPanel(false);
-      navigate("/captain-riding", { state: { ride: props.ride } });
-    }
   };
   return (
     <div>
@@ -49,8 +30,11 @@ const ConfirmRidePopUp = (props) => {
             src="https://i.pinimg.com/236x/af/26/28/af26280b0ca305be47df0b799ed1b12b.jpg"
             alt=""
           />
-          <h2 className="text-lg font-medium capitalize">
+          {/* <h2 className="text-lg font-medium capitalize">
             {props.ride?.user.fullname.firstname}
+          </h2> */}
+          <h2 className="text-lg font-medium capitalize">
+            Achint Jain
           </h2>
         </div>
         <h5 className="text-lg font-semibold">2.2 KM</h5>
@@ -62,8 +46,11 @@ const ConfirmRidePopUp = (props) => {
             <div>
               <h3 className="text-lg font-medium">562/11-A</h3>
               <p className="text-sm -mt-1 text-gray-600">
-                {props.ride?.pickup}
+                bhind
               </p>
+              {/* <p className="text-sm -mt-1 text-gray-600">
+                {props.ride?.pickup}
+              </p> */}
             </div>
           </div>
           <div className="flex items-center gap-5 p-3 border-b-2">
@@ -71,14 +58,18 @@ const ConfirmRidePopUp = (props) => {
             <div>
               <h3 className="text-lg font-medium">562/11-A</h3>
               <p className="text-sm -mt-1 text-gray-600">
-                {props.ride?.destination}
+                banglore
               </p>
+              {/* <p className="text-sm -mt-1 text-gray-600">
+                {props.ride?.destination}
+              </p> */}
             </div>
           </div>
           <div className="flex items-center gap-5 p-3">
             <i className="ri-currency-line"></i>
             <div>
-              <h3 className="text-lg font-medium">₹{props.ride?.fare} </h3>
+              {/* <h3 className="text-lg font-medium">₹{props.ride?.fare} </h3> */}
+              <h3 className="text-lg font-medium">₹259</h3>
               <p className="text-sm -mt-1 text-gray-600">Cash Cash</p>
             </div>
           </div>
@@ -94,9 +85,9 @@ const ConfirmRidePopUp = (props) => {
               placeholder="Enter OTP"
             />
 
-            <button className="w-full mt-5 text-lg flex justify-center bg-green-600 text-white font-semibold p-3 rounded-lg">
+            <Link to='/captain-riding' className="w-full mt-5 text-lg flex justify-center bg-green-600 text-white font-semibold p-3 rounded-lg">
               Confirm
-            </button>
+            </Link>
             <button
               onClick={() => {
                 props.setConfirmRidePopupPanel(false);
