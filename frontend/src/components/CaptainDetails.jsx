38516a6279ceb12fc9ac9552d409a52695cf9667
achint <achintj96244@gmail.com>
2025-08-17 14:11:45 +0530

    Update CaptainDetails component to use static captain name and image

--- frontend/src/components/CaptainDetails.jsx
+++ frontend/src/components/CaptainDetails.jsx
@@ -1,8 +1,8 @@
-import React, { useContext } from "react";
-import { CaptainDataContext } from "../context/CapatainContext";
+// import React, { useContext } from "react";
+// import { CaptainDataContext } from "../context/CapatainContext";
 
 const CaptainDetails = () => {
-  const { captain } = useContext(CaptainDataContext);
+  // const { captain } = useContext(CaptainDataContext);
 
   return (
     <div>
@@ -10,12 +10,13 @@ const CaptainDetails = () => {
         <div className="flex items-center justify-start gap-3">
           <img
             className="h-10 w-10 rounded-full object-cover"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdlMd7stpWUCmjpfRjUsQ72xSWikidbgaI1w&s"
+            src="https://animegenius-global.live3d.io/vtuber/ai_product/anime_genius/static/imgs/cover_adfec2f98c5418cdf818bc4cfba4c307.webp"
             alt=""
           />
-          <h4 className="text-lg font-medium capitalize">
+          {/* <h4 className="text-lg font-medium capitalize">
             {captain.fullname.firstname + " " + captain.fullname.lastname}
-          </h4>
+          </h4> */}
+          <h4>Achint Jain</h4>
         </div>
         <div>
           <h4 className="text-xl font-semibold">₹295.20</h4>
