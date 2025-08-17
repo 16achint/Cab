694754a698938c045eb0af75dfe6161aa9c6f10b
achint <achintj96244@gmail.com>
2025-08-17 20:31:22 +0530

    Add ESLint configuration for code quality and style enforcement

--- backend/app.js
+++ backend/app.js
@@ -16,12 +16,15 @@ app.use(cookieParser());
 import userRoutes from "./routes/user.route.js";
 import captionRoutes from "./routes/caption.route.js";
 import mapRoutes from "./routes/map.routes.js";
+import rideRoutes from "./routes/ride.route.js";
+
 app.use("/users", userRoutes);
 app.use("/captain", captionRoutes);
 app.use("/map", mapRoutes);
+app.use("/ride", rideRoutes);
 
 app.get("/", (req, res) => {
-  res.send("server is healthy 🍀");
+    res.send("server is healthy 🍀");
 });
 
 export default app;
