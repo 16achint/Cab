191c729cc52b6151dd99bae7ce2fdb717a7eadbb
achint <achintj96244@gmail.com>
2025-08-21 18:38:38 +0530

    feat: initialize socket connection in server setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import { createServer } from "http";
 import app from "./app.js";
 import { log } from "console";
+import { initializeSocket } from "./socket.js";
+
 const port = process.env.PORT || 3000;
 const server = createServer(app);
+initializeSocket(server);
 
 server.listen(port, () => {
-  log("server listening on port", port);
+    log("server listening on port", port);
 });
