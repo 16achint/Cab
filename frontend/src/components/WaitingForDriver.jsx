e4b95288ea5fa8a16172cfe79ffa3c20b1d24a0d
achint <achintj96244@gmail.com>
2025-08-22 01:27:53 +0530

    fix: update click handler to log message and correctly set waiting state

--- frontend/src/components/WaitingForDriver.jsx
+++ frontend/src/components/WaitingForDriver.jsx
@@ -6,7 +6,8 @@ const WaitingForDriver = (props) => {
       <h5
         className="p-1 text-center w-[93%] absolute top-0"
         onClick={() => {
-          props.waitingForDriver(false);
+          console.log('hello world')
+          props.setWaitingForDriver(false);
         }}
       >
         <i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i>
