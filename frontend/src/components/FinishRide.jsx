639c2f0f168c3e2cc8ceafcdb3f1272fb4af38f7
achint <achintj96244@gmail.com>
2025-08-22 15:28:37 +0530

    fix: restore dynamic user and ride details in FinishRide component

--- frontend/src/components/FinishRide.jsx
+++ frontend/src/components/FinishRide.jsx
@@ -27,11 +27,8 @@ const FinishRide = (props) => {
             src="https://i.pinimg.com/236x/af/26/28/af26280b0ca305be47df0b799ed1b12b.jpg"
             alt=""
           />
-          {/* <h2 className="text-lg font-medium">
-            {props.ride?.user.fullname.firstname}
-          </h2> */}
           <h2 className="text-lg font-medium">
-            dumkiee
+            {props.ride?.user.fullname.firstname}
           </h2>
         </div>
         <h5 className="text-lg font-semibold">2.2 KM</h5>
@@ -42,31 +39,25 @@ const FinishRide = (props) => {
             <i className="ri-map-pin-user-fill"></i>
             <div>
               <h3 className="text-lg font-medium">562/11-A</h3>
-              {/* <p className="text-sm -mt-1 text-gray-600">
-                {props.ride?.pickup}
-              </p> */}
               <p className="text-sm -mt-1 text-gray-600">
-                bhopal
+                {props.ride?.pickup}
               </p>
+
             </div>
           </div>
           <div className="flex items-center gap-5 p-3 border-b-2">
             <i className="text-lg ri-map-pin-2-fill"></i>
             <div>
               <h3 className="text-lg font-medium">562/11-A</h3>
-              {/* <p className="text-sm -mt-1 text-gray-600">
-                {props.ride?.destination}
-              </p> */}
               <p className="text-sm -mt-1 text-gray-600">
-                bhopal
+                {props.ride?.destination}
               </p>
             </div>
           </div>
           <div className="flex items-center gap-5 p-3">
             <i className="ri-currency-line"></i>
             <div>
-              {/* <h3 className="text-lg font-medium">₹{props.ride?.fare} </h3> */}
-              <h3 className="text-lg font-medium">₹298 </h3>
+              <h3 className="text-lg font-medium">₹{props.ride?.fare} </h3>
               <p className="text-sm -mt-1 text-gray-600">Cash Cash</p>
             </div>
           </div>
