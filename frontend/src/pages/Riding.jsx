ec7ef63295f0b22b3b24bb0d3b17b13296100947
achint <achintj96244@gmail.com>
2025-08-22 15:28:55 +0530

    fix: clean up Riding component by restoring dynamic ride details and removing commented code

--- frontend/src/pages/Riding.jsx
+++ frontend/src/pages/Riding.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import LiveTracking from "../components/LiveTracking"
-import { Link } from 'react-router-dom' // Added useLocation
+import { Link, useLocation } from 'react-router-dom' // Added useLocation
 
 const Riding = () => {
-
+    const location = useLocation()
     const { ride } = location.state || {} // Retrieve ride data
 
-
-
+    console.log("ride here => ", ride)
     return (
         <div className='h-screen'>
             <Link to='/home' className='fixed right-2 top-2 h-10 w-10 bg-white flex items-center justify-center rounded-full'>
@@ -21,12 +20,9 @@ const Riding = () => {
                 <div className='flex items-center justify-between'>
                     <img className='h-12' src="https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg" alt="" />
                     <div className='text-right'>
-                        {/* <h2 className='text-lg font-medium capitalize'>{ride?.captain.fullname.firstname}</h2>
+                        <h2 className='text-lg font-medium capitalize'>{ride?.captain.fullname.firstname}</h2>
                         <h4 className='text-xl font-semibold -mt-1 -mb-1'>{ride?.captain.vehicle.plate}</h4>
-                        <p className='text-sm text-gray-600'>Maruti Suzuki Alto</p> */}
-                        <h2 className='text-lg font-medium capitalize'>Achint</h2>
-                        <h4 className='text-xl font-semibold -mt-1 -mb-1'>mp 30 mv 9415</h4>
-                        <p className='text-sm text-gray-600'>Royal enfield classic 350</p>
+                        <p className='text-sm text-gray-600'>{ride?.captain.vehicle.vehicleType}</p>
 
                     </div>
                 </div>
@@ -42,10 +38,10 @@ const Riding = () => {
                             </div>
                         </div>
                         <div className='flex items-center gap-5 p-3'>
-                            <i className="ri-currency-line"></i>
+                            <i className="ri-currency-line" />
                             <div>
                                 <h3 className='text-lg font-medium'>₹{ride?.fare} </h3>
-                                <p className='text-sm -mt-1 text-gray-600'>Cash Cash</p>
+                                <p className='text-sm -mt-1 text-gray-600'>Cash</p>
                             </div>
                         </div>
                     </div>
